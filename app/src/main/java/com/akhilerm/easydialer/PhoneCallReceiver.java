5c9c1c74f0493778e14a1c2476c893675ab7d3c5
Akhil Mohan <akhilerm@gmail.com>
2018-08-21 16:01:08 +0400

    code area to edit call log made.
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- app/src/main/java/com/akhilerm/easydialer/PhoneCallReceiver.java
+++ app/src/main/java/com/akhilerm/easydialer/PhoneCallReceiver.java
@@ -22,7 +22,7 @@ public class PhoneCallReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        /*dialerSettings = new DialerSettings(context);
+        dialerSettings = new DialerSettings(context);
         Log.d(TAG,"Captured Outgoing Call");
         String dialedNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
         Log.d(TAG, "Dialled Number : " + dialedNumber);
@@ -37,10 +37,67 @@ public class PhoneCallReceiver extends BroadcastReceiver {
             Log.d(TAG, "Formatted Number : " + formattedNumber);
             outgoingCall.setData(Uri.parse("tel:"+ Uri.encode(formattedNumber)));
             context.startService(outgoingCall);
-        }*/
+        }
         TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        CustomPhoneStateListener customPhoneStateListener = new CustomPhoneStateListener(context);
+        CustomPhoneStateListener customPhoneStateListener = new CustomPhoneStateListener();
         telephonyManager.listen(customPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+    public class CustomPhoneStateListener extends PhoneStateListener {
+        private final String TAG = CustomPhoneStateListener.class.getName() + ":DEBUG:";
+        private int previousState = TelephonyManager.CALL_STATE_IDLE;
+        private boolean isIncoming;
+
+        @Override
+        public void onCallStateChanged(int state, String phoneNumber) {
+            if (phoneNumber != null && !phoneNumber.isEmpty()) {
+                Log.d(TAG, "Phone nummber : " + phoneNumber);
+            }
+            if (previousState == state) {
+                return;
+            }
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    if (previousState == TelephonyManager.CALL_STATE_IDLE) {
+                        Log.d(TAG, "Incoming call " + phoneNumber);
+                        previousState = state;
+                        isIncoming = true;
+                    }
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    if (previousState == TelephonyManager.CALL_STATE_IDLE) {
+                        Log.d(TAG, "outgoing call " + phoneNumber);
+                        previousState = state;
+                        isIncoming = false;
+                    }
+                    else if (previousState == TelephonyManager.CALL_STATE_RINGING) {
+                        Log.d(TAG, "Took incoming call " + phoneNumber);
+                        previousState = state;
+                    }
+                    break;
+                case TelephonyManager.CALL_STATE_IDLE:
+                    if (previousState == TelephonyManager.CALL_STATE_RINGING) {
+                        Log.d(TAG, "Missed call " + phoneNumber);
+                        previousState = state;
+                    }
+                    else if (previousState == TelephonyManager.CALL_STATE_OFFHOOK) {
+                        if (isIncoming) {
+                            Log.d(TAG, "Incoming call ended " + phoneNumber);
+                        }
+                        else {
+                            Log.d(TAG, "Outgoing call ended " + phoneNumber);
+                            onOutgoingCallEnded(phoneNumber);
+                        }
+                        previousState = state;
+                    }
+                    break;
+            }
+        }
+
+
+        private void onOutgoingCallEnded(String number) {
+            // Code to edit call log
+        }
     }
 
 }
