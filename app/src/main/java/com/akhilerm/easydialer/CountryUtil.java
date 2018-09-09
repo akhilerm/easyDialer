999b27a7a70ddaff3375dcb5b4c6d6f2b4215daa
Akhil Mohan <akhilerm@gmail.com>
2018-09-09 20:10:52 +0400

    Making country utils for country code handling
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- app/src/main/java/com/akhilerm/easydialer/CountryUtil.java
+++ app/src/main/java/com/akhilerm/easydialer/CountryUtil.java
@@ -1,31 +1,41 @@
 package com.akhilerm.easydialer;
 
 import java.util.HashMap;
+import java.util.Locale;
 
 public class CountryUtil {
 
+
     static String getISOCode(String phoneNumber) {
+
         phoneNumber=phoneNumber.replace("+","");
+        HashMap <String, String> countryMap = getCountryMap();
+
         if (phoneNumber.startsWith("91")||phoneNumber.startsWith("0091"))
             return "IN";
         return "CCD"; //calling card
+
     }
 
     static int getISDCode(String ISOCode) {
-       if(ISOCode.equals("IN")) {
-           return 91;
-       }
-       return 0;
+       return getCountryMap().get(ISOCode);
     }
 
     static String getCountryName(String ISOCode) {
-        if (ISOCode.equals("IN")) {
-            return "India";
-        }
-        return "NOt found";
+        Locale loc = new Locale("",ISOCode);
+        return loc.getDisplayCountry();
     }
 
     static String[] getCountryArray(String countries) {
         return countries.split(",");
+    }
+
+    private static HashMap getCountryMap() {
+        HashMap<String, String> countryMap = new HashMap<>();
+        countryMap.put("IN","91");
+        countryMap.put("PK","92");
+        countryMap.put("LK","94");
+        countryMap.put("BD","880");
+        return countryMap;
     }
 }
